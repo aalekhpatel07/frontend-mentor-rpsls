872689322e9f9b38bd3e7171c36ff85a013e14e7
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-11 06:15:05 -0600

    Add the Pentagon component that houses all the action components.

---
+++ src/components/Pentagon.svelte
@@ -0,0 +1,185 @@
+<script>
+    // import Pentagon from '../../images/bg-pentagon.svg';
+
+    import Action from './Action.svelte';
+
+    import Scissors from '../../images/icon-scissors.svg';
+    import Paper from '../../images/icon-paper.svg';    
+    import Rock from '../../images/icon-rock.svg';
+    import Lizard from '../../images/icon-lizard.svg';
+    import Spock from '../../images/icon-spock.svg';
+
+
+    const icons = {
+        scissors: Scissors,
+        paper: Paper,
+        rock: Rock,
+        lizard: Lizard,
+        spock: Spock
+    }
+
+
+    const gradients = {
+        scissors: "hsl(39, 89%, 43%) to hsl(44, 84%, 50%)",
+        paper: "hsl(240, 89%, 62%) to hsl(230, 89%, 65%)",
+        rock: "hsl(349, 71%, 42%) to hsl(349, 70%, 56%)",
+        lizard: "hsl(261, 78%, 60%) to hsl(261, 72%, 63%)",
+        spock: "hsl(199, 59%, 53%) to hsl(189, 58%, 57%)"
+    };
+
+    function handleActionSelected(e){
+        const name = e.detail.name;
+    }
+
+</script>
+<style>
+
+    .scissors{
+        top: -2rem;
+        left: 3rem;
+    }
+    .rock{
+        top: 9rem;
+        right: 0;
+    }
+    .paper{
+        top: 2rem;
+        right: -3rem;
+    }
+    .lizard{
+        top: 9rem;
+        left: -1rem;
+    }
+    .spock{
+        top: 2rem;
+        left: -3rem;
+    }
+    .pentagon{
+        width: 200px;
+    }
+    .pentagon-container{
+        margin-top: 4rem;
+    }
+
+    @media screen and (min-width: 421px){
+        .scissors{
+            top: -2rem;
+            left: calc(150px - 3rem);
+        }
+        .rock{
+            top: 13.5rem;
+            right: 0;
+        }
+        .paper{
+            top: 3.5rem;
+            right: -2rem;
+        }
+        .lizard{
+            top: 13.5rem;
+            left: 0;
+        }
+        .spock{
+            top: 3.5rem;
+            left: -2rem;
+        }
+        .pentagon{
+            width: 300px;
+        }
+        /* .pentagon-container{
+            margin-top: 0;
+            min-height: 100vh;
+        } */
+    }
+    @media screen and (min-width: 768px){
+        .scissors{
+            top: -3rem;
+            left: calc(200px - 3rem);
+        }
+        .rock{
+            top: 18rem;
+            right: 2rem;
+        }
+        .paper{
+            top: 5rem;
+            right: -3rem;
+        }
+        .lizard{
+            top: 18rem;
+            left: 2rem;
+        }
+        .spock{
+            top: 5rem;
+            left: -3rem;
+        }
+        .pentagon{
+            width: 400px;
+        }
+        /* .pentagon-container{
+            margin-top: 6rem;
+        } */
+    }
+    @media screen and (min-width: 1024px){
+       .pentagon-container{
+            margin-top: 4rem;
+        } 
+    }
+    @media screen and (min-width: 1366px){
+       .pentagon-container{
+            margin-top: 4rem;
+        } 
+    }
+    
+</style>
+<div class="my-16">
+    <div
+        class="flex relative mx-auto pentagon-container border-4 border-red-600"
+    >
+        <img
+            class="pentagon"
+            src="images/bg-pentagon.svg"
+            alt="Pentagon"
+        />
+        <div
+            class="absolute scissors"
+        >
+            <Action
+                icon={icons.scissors}
+                gradient={gradients.scissors}
+                name={'scissors'}
+                on:action-selected={handleActionSelected}
+            /> 
+        </div>
+        <div class="absolute paper">
+            <Action
+                icon={icons.paper}
+                gradient={gradients.paper}
+                name={'paper'}
+                on:action-selected={handleActionSelected}
+            />
+        </div>
+        <div class="absolute rock">
+            <Action
+                icon={icons.rock}
+                gradient={gradients.rock}
+                name={'rock'}
+                on:action-selected={handleActionSelected}
+            /> 
+        </div>
+        <div class="absolute lizard">
+            <Action
+                icon={icons.lizard}
+                gradient={gradients.lizard}
+                name={'lizard'}
+                on:action-selected={handleActionSelected}
+            />
+        </div>
+        <div class="absolute spock">
+            <Action
+                icon={icons.spock}
+                gradient={gradients.spock}
+                name={'spock'}
+                on:action-selected={handleActionSelected}
+            /> 
+        </div>
+    </div>
+</div>
