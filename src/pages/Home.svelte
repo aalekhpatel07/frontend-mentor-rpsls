8cb4e8b0b5f45fab731b6899dd81f73de29cee9a
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-11 13:52:19 -0600

    Fix some flex styling issues.

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -10,16 +10,19 @@
 <style>
 </style>
 <Backdrop>
-    <div class="min-h-screen min-w-full pt-3 flex flex-col justify-start items-between">
+    <div class="min-h-screen min-w-full pt-3 flex flex-col justify-start md:justify-start items-between">
         <div class="flex justify-center">
             <HeaderPanel
                 score={score}
             />
         </div>
-        <div class="flex justify-center">
+        <div
+            class="flex justify-center max-w-4xl mx-auto mt-12 md:mt-0 "
+            style="overflow: none;"
+        >
             <Pentagon />
         </div>
-        <div class="flex justify-center mt-16 md:justify-end md:mr-10 md:-mt-16">
+        <div class="flex justify-center mt-8 md:justify-end md:mr-10 md:-mt-16">
             <Rules />
         </div>
     </div>
