7d8675926d539381b9f885fd04d564c85011cfd4
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-13 12:48:07 -0600

    Add icon data for actions.

---
+++ src/@compute/data.js
@@ -0,0 +1,23 @@
+import Scissors from '../../images/icon-scissors.svg';
+import Paper from '../../images/icon-paper.svg';    
+import Rock from '../../images/icon-rock.svg';
+import Lizard from '../../images/icon-lizard.svg';
+import Spock from '../../images/icon-spock.svg';
+
+
+export const icons = {
+    scissors: Scissors,
+    paper: Paper,
+    rock: Rock,
+    lizard: Lizard,
+    spock: Spock
+}
+
+
+export const gradients = {
+    scissors: "hsl(39, 89%, 43%) to hsl(44, 84%, 50%)",
+    paper: "hsl(240, 89%, 62%) to hsl(230, 89%, 65%)",
+    rock: "hsl(349, 71%, 42%) to hsl(349, 70%, 56%)",
+    lizard: "hsl(261, 78%, 60%) to hsl(261, 72%, 63%)",
+    spock: "hsl(199, 59%, 53%) to hsl(189, 58%, 57%)"
+};
