8a6be6d90b734e174ce1f6f6e7cb667043e92d1b
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-14 14:47:32 -0600

    Add button for playing again.

---
+++ src/components/PlayAgainButton.svelte
@@ -0,0 +1,20 @@
+<script>
+
+    const bgColor = 'white';
+    const textColor = 'hsl(214, 47%, 23%)';
+
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    function handlePlayAgain(){
+        dispatch('play-again')
+    }
+</script>
+<button
+    type="button"
+    class="uppercase barlow shadow-2xl whitespace-nowrap text-md md:text-lg text-center w-48 md:w-60 h-12 rounded-xl tracking-widest font-semibold"
+    style="background: {bgColor}; color: {textColor};"
+    on:click={handlePlayAgain}
+>
+    <span>play again</span>
+</button>
