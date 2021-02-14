699b95d662aa52eab6588995aacd449c51c6d31f
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-14 17:39:23 -0600

    Add variant as a parameter of the game engine.

--- src/@compute/move.js
+++ src/@compute/move.js
@@ -1,10 +1,15 @@
 
 import { icons } from '../@compute/data';
 
-const fetchRandomAction = () => {
-    const keys = Object.keys(icons)
+const fetchRandomAction = (variant) => {
+    let keys;
+    if(variant == 'rps'){
+        keys = Object.keys(icons.rps)
+    }else{
+        keys = Object.keys(icons.rpsls)
+    }
     const idx = Math.floor(Math.random() * keys.length)
     return keys[idx]
 }
 
-export const move = fetchRandomAction;
+export const move = (variant) => fetchRandomAction(variant);
