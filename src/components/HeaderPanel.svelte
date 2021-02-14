1f2b096c22c268944d98146391267e6257ffbda8
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-14 17:43:06 -0600

    Reflect the textual changes of the variant in the header.

--- src/components/HeaderPanel.svelte
+++ src/components/HeaderPanel.svelte
@@ -8,7 +8,7 @@
     
     const letterSpacing = '2px';
 
-    export let score, negative;
+    export let score, negative, variant;
     
 
 </script>
@@ -30,16 +30,20 @@
 </style>
 
 <div
-    class="border-4 flex justify-between w-4/6 "
+    class="border-4 flex justify-between w-4/6 items-center "
     style="border-radius: {borderRadius}; --scale: {scale}; border-color: {borderColor};"
 >
-    <div class="flex flex-col justify-center py-6 px-8 w-2/5 sm:w-1/2 md:w-2/3 lg:w-3/4 text-white">
-        <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Rock</span>
-        <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Paper</span>
-        <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Scissors</span>
-        <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Lizard</span>
-        <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Spock</span>
-    </div>
+
+    
+        <div class="flex flex-col justify-center py-6 px-8 w-2/5 sm:w-1/2 md:w-2/3 lg:w-3/4 text-white h-32">
+            <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Rock</span>
+            <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Paper</span>
+            <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Scissors</span>
+            {#if variant === 'rpsls'}
+                <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Lizard</span>
+                <span class="barlow text-xs md:text-xl transform-y-scale uppercase font-bold">Spock</span>
+            {/if}
+        </div>
     <div class="flex flex-col justify-center p-6 w-3/5 sm:w-1/2 md:w-1/3 lg:w-1/4">
         <div
             class="bg-white h-100 py-3 md:py-6 lg:py-4 rounded-lg flex flex-col align-center justify-center"
