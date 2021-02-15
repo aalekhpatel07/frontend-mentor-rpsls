5652f68a10b8b4ab415a5c3403e50b096a5da9e8
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2021-02-15 15:19:41 -0600

    Integrate chrome.storage API and add manifests and background script for it to function as a chrome extension.

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,12 @@
 import { writable } from 'svelte/store'
 
-let storedScore = localStorage.getItem('rpsls-score')
+let storedScore = localStorage.getItem('rpsls_score')
+if(chrome && chrome.storage){
+    chrome.storage.sync.get(['rpsls_score'], (result) => {
+        storedScore = result
+    })
+}
+
 if(storedScore == null || storedScore == undefined){
     storedScore = 0
 }else{
@@ -10,5 +16,9 @@ if(storedScore == null || storedScore == undefined){
 export const score = writable(storedScore)
 
 score.subscribe(value => {
-    localStorage.setItem('rpsls-score', (value === null || value === undefined) ? 0 : value)
+    const to_set = (value === null || value === undefined) ? 0 : value
+    localStorage.setItem('rpsls_score', to_set)
+    if(chrome && chrome.storage){
+        chrome.storage.sync.set({rpsls_score: to_set})
+    }
 })
